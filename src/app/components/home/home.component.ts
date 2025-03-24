f847647527219666b51c623d2bf380ebbb52e682
= <vvaleria26@gmail.com>
2025-03-24 04:50:39 -0500

    se agregaron fotos y ahora si se muestran, desde Angular 17 ahora se estandarizó utilizar la capeta public para guardar y acceder a contenido media

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -20,15 +20,26 @@ export class HomeComponent {
       day: 'Día 1',
       description: 'El gatito inicia su tratamiento hoy. Se muestra animado y con signos de recuperación.',
       mediaItems: [
-        { type: 'image', url: '../../../assets/media/11.jpeg' },
-        { type: 'video', url: '../../../assets/media/Dia1/vid1.mp4' },
+        { type: 'image', url: 'media/Dia1/11.jpeg' },
+        { type: 'image', url: 'media/Dia1/12.jpeg' },
+        { type: 'image', url: 'media/Dia1/13.jpeg' },
+        { type: 'image', url: 'media/Dia1/14.jpeg' },
+        { type: 'image', url: 'media/Dia1/15.jpeg' },
       ] as MediaItem[]
     },
     {
       day: 'Día 2',
       description: 'El gatito continúa mejorando. Se observan cambios positivos en su comportamiento.',
       mediaItems: [
-        { type: 'image', url: 'assets/media/foto2.jpg' }
+        { type: 'image', url: 'media/Dia2/21.jpeg' },
+        { type: 'video', url: 'media/Dia2/vid1.mp4' },
+        { type: 'image', url: 'media/Dia2/22.jpg' },
+        { type: 'image', url: 'media/Dia2/23.jpeg' },
+        { type: 'image', url: 'media/Dia2/24.jpeg' },
+        { type: 'image', url: 'media/Dia2/25.jpeg' },
+        { type: 'image', url: 'media/Dia2/26.jpeg' },
+        { type: 'image', url: 'media/Dia2/27.jpeg' },
+        { type: 'image', url: 'media/Dia2/28.jpeg' },
       ] as MediaItem[]
     }
     // Puedes agregar más días según sea necesario.
